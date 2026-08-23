97a9050a37d42be8d03f2a5da53039e735ab537c
Rafael <jorgesernaquech@gmail.com>
2026-08-23 13:52:36 -0500

    FrmCobroCliente terminado

--- src/main/java/com/ferronor/sic/compras/vista/FrmCompras.java
+++ src/main/java/com/ferronor/sic/compras/vista/FrmCompras.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package com.ferronor.sic.compras.vista;
 
 import com.ferronor.sic.compras.logica.OrdenCompraService;
